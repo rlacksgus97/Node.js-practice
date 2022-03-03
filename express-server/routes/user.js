d340c84b37c9980a5038f62962dd6e473fa66149
rlacksgus97 <rlacksgus97@naver.com>
2022-03-03 13:06:39 +0900

    feat: router

---
+++ express-server/routes/user.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const router = express.Router();
+
+// GET /user 라우터
+router.get('/', (req, res) => {
+    res.send('Hello, User');
+});
+
+router.get('/:id', (req, res) => {
+    console.log(req.params, req.query);
+});
+
+module.exports = router;
