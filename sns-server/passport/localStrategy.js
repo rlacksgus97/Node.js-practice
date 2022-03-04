1469b10ce2ecabeca638aae0d7c62f18f2408824
rlacksgus97 <rlacksgus97@naver.com>
2022-03-04 13:26:00 +0900

    feat: passport, login

--- sns-server/passport/localStrategy.js
+++ sns-server/passport/localStrategy.js
@@ -1,6 +1,6 @@
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
-const bcrypt = requrire('bcrypt');
+const bcrypt = require('bcrypt');
 
 const User = require('../models/user');
 
--- sns-server/routes/auth.js
+++ sns-server/routes/auth.js
@@ -26,7 +26,7 @@ router.post('/join', isNotLoggedIn, async (req, res, next) => {
     }
 });
 
-router.pose('/login', isNotLoggedIn, (req, res, next) => {
+router.post('/login', isNotLoggedIn, (req, res, next) => {
     passport.authenticate('local', (authError, user, info) => {
         if (authError) {
             console.error(authError);
