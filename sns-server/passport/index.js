f5559191b9f5924693eaf6b04aed7548e0e93e0a
rlacksgus97 <rlacksgus97@naver.com>
2022-03-08 17:15:00 +0900

    feat: caching deserializeUser

--- sns-server/passport/index.js
+++ sns-server/passport/index.js
@@ -3,6 +3,8 @@ const local = require('./localStrategy');
 // const kakao = require('./kakaoStrategy');
 const User = require('../models/user');
 
+let cachedUser;
+
 module.exports = () => {
     //serializeUser : 로그인 시 실행
     passport.serializeUser((user, done) => {
@@ -11,22 +13,32 @@ module.exports = () => {
     });
 
     //desrializeUser : 매 요청 시 실행
-    passport.deserializeUser((id, done) => {
-        User.findOne({
-            where: {id},
-            include: [{
-                model: User,
-                attributes: ['id', 'nick'],
-                as: 'Followers',
-            }, {
-                model: User,
-                attributes: ['id', 'nick'],
-                as: 'Followings',
-            }],
-        })
-        //done()으로 req.user에 user 저장
-        .then(user => done(null, user))
-        .catch(err => done(err));
+    passport.deserializeUser(async (id, done) => {
+        console.log('hihi1');
+        console.log(cachedUser);
+        if (!cachedUser) {
+            console.log('hihi2');
+            const user = await User.findOne({
+                where: {id},
+                include: [{
+                    model: User,
+                    attributes: ['id', 'nick'],
+                    as: 'Followers',
+                }, {
+                    model: User,
+                    attributes: ['id', 'nick'],
+                    as: 'Followings',
+                }],
+            })
+            //done()으로 req.user에 user 저장
+            // .then(user => done(null, user))
+            // .catch(err => done(err));
+            cachedUser = user;
+            done(null, user);
+        } else {
+            console.log('hihi3');
+            done(null, cachedUser);
+        } 
     });
 
     local();
